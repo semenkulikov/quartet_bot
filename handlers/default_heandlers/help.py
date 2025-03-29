ac6f5fc92521ea8b1a08d400eb022ca1f32f2c41
Semen Saifutdinov <saifutdinov2006@mail.ru>
2025-03-29 12:51:11 +0300

    Added docstring to help handler

--- handlers/default_heandlers/help.py
+++ handlers/default_heandlers/help.py
@@ -7,6 +7,7 @@ from config_data.config import DEFAULT_COMMANDS, ADMIN_COMMANDS, ALLOWED_USERS
 
 @dp.message(Command('help'))
 async def bot_help(message: types.Message):
+    """ Хендлер для выдачи справки по боту """
     commands = [f"/{cmd} - {desc}" for cmd, desc in DEFAULT_COMMANDS]
     if int(message.from_user.id) in ALLOWED_USERS:
         commands.extend([f"/{cmd} - {desc}" for cmd, desc in ADMIN_COMMANDS])

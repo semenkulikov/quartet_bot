77c50d95677d7d3e00faaf934ac68fe465d2c4f8
Semen Saifutdinov <saifutdinov2006@mail.ru>
2025-03-30 14:54:57 +0300

    Added docstring to echo handler

--- handlers/default_heandlers/echo.py
+++ handlers/default_heandlers/echo.py
@@ -3,6 +3,7 @@ from loader import dp
 
 @dp.message()
 async def bot_echo(message: types.Message):
+    """ Хендлер для обработки всех остальных сообщений """
     await message.reply(
         "Введите любую команду из меню, чтобы я начал работать\n"
         "Либо выберите одну из кнопок, которые я вам прислал"

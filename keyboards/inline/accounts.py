e7a41939297ca5038dff853d624ef2f07001f649
Semen Saifutdinov <saifutdinov2006@mail.ru>
2025-04-15 16:48:42 +0300

    Added new inline buttons

--- keyboards/inline/accounts.py
+++ keyboards/inline/accounts.py
@@ -1,14 +1,14 @@
 from aiogram.types import InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-from config_data.config import ADMIN_ID
-from database.query_orm import get_all_users
+from config_data.config import ADMIN_IDS
+from database.db import get_all_users
 
 async def users_markup() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     users = await get_all_users()
     for user in users:
         # Добавляем кнопку только если пользователь не является администратором
-        if int(user.user_id) != int(ADMIN_ID):
+        if int(user.user_id) not in ADMIN_IDS:
             builder.button(text=user.username, callback_data=str(user.id))
     # Добавляем кнопку "Выйти"
     builder.button(text="Выйти", callback_data="Выход")

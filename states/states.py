61843cba5af1eb97e5e35dacadaf1cb891436482
Semen Saifutdinov <saifutdinov2006@mail.ru>
2025-04-15 16:48:22 +0300

    Added new states

--- states/states.py
+++ states/states.py
@@ -1,5 +1,21 @@
 from aiogram.fsm.state import StatesGroup, State
 
 
+class AdminStates(StatesGroup):
+    managing_factions = State()
+    managing_games = State()
+    creating_faction = State()
+    adding_cards = State()
+    deleting_faction = State()
+    viewing_faction = State()
+    viewing_game = State()
+    ending_game = State()
+
 class AdminPanel(StatesGroup):
     get_users = State()
+
+
+class GameStates(StatesGroup):
+    waiting_for_players = State()
+    in_game = State()
+    requesting_card = State()

4a9655e0db641c1ec63b7bfaf9e9e691dcd5e741
Semen Saifutdinov <saifutdinov2006@mail.ru>
2025-04-20 12:14:41 +0300

    Added docstring to middlewares

--- middlewares/db.py
+++ middlewares/db.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.asyncio import async_sessionmaker
 
 
 class DataBaseSession(BaseMiddleware):
+    """ Миддлвэр для создания сессии по работе с БД и передачи ее в хендлеры """
     def __init__(self, session_pool: async_sessionmaker):
         self.session_pool = session_pool
 

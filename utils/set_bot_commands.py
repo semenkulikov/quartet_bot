630ccc6631456aeeebd1cfdd7a3bfb349a52b788
Semen Saifutdinov <saifutdinov.semen@mail.ru>
2025-04-28 14:35:07 +0300

    Added docstring to utils

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -3,5 +3,6 @@ from config_data.config import DEFAULT_COMMANDS
 from loader import bot
 
 async def set_default_commands():
+    """ Функция для установки команд по умолчанию в бота """
     commands = [BotCommand(command=cmd, description=desc) for cmd, desc in DEFAULT_COMMANDS]
     await bot.set_my_commands(commands)
